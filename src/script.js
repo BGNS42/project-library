fabc18ce22fa373d7396e133b00da4a4a00dc3b1
BGNS42 <196232533+BGNS42@users.noreply.github.com>
2025-10-13 15:12:33 -0300

    JavaScript book constructor and addBookToLibrary function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
-console.log("Project Skeleton");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if(!new.target) {
+        throw new Error("You must use the 'new' operator to call the constructor")
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+const harryPotter = new Book("Harry Potter e a Pedra Filosofal", "J.K. Rolling", "235", "Not read yet");
+const senhorDosAneis = new Book("Senhor dos Aneis e as Duas Torres", "J.R.R. Tolkien", "554", "Read");
+
+addBookToLibrary(harryPotter);
+addBookToLibrary(senhorDosAneis);
+
+console.log(myLibrary);
